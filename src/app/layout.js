eb8a206627c53812bd88a60360df60393e3670f1
Uziel Trujillo <uziel.trujillo@gmail.com>
2024-10-31 13:16:41 -0600

    Agregando tipografia Open Sans 	modified:   layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,6 @@
-import localFont from "next/font/local";
+import { Open_Sans } from 'next/font/google'
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const opensans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +11,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={`${opensans.className} antialiased`}
 			>
 				{children}
 			</body>
