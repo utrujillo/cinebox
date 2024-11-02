9f8b33c056805003314823542e8fb3527d93a6d6
Uziel Trujillo <uziel.trujillo@gmail.com>
2024-11-01 22:48:24 -0600

    Refactorizando el componente Busqueda 	modified:   components/Busqueda.js

--- src/app/components/Busqueda.js
+++ src/app/components/Busqueda.js
@@ -5,27 +5,38 @@ import lupa from '@/app/assets/icons/lupa.png'
 
 const Busqueda = () => {
 
+    const [ buscar, setBuscar ] = useState('')
     const [ visible, setVisible ] = useState(false)
-    const [ peliculas, setPeliculas ] = useState('')
-    const [ series, setSeries ] = useState('')
     const toggleInput = () => { setVisible(!visible) }
-    const active = (el) => {
-        if (el == 'peliculas') {
-            setPeliculas( peliculas.length == 0 ? 'font-bold' : '' )
-            setSeries('')
-        }else if ( el === 'series' ){
-            setSeries( series.length === 0 ? 'font-bold' : '' )
-            setPeliculas('')
-        }
-    }
+
+    let opciones = ['Peliculas', 'Series']
+    const [ categoriaActiva, setCategoriaActiva ] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        console.log(`Buscar en ${categoriaActiva} el nombre ${buscar}`)
+    } 
 
     return (
         <div className='w-1/2 bg-white rounded-3xl text-[#2F3139] h-16 p-4 flex items-center justify-between'>
             {
                 !visible &&
                 <>
-                    <div className={'cursor-pointer hover:bg-gray-100 h-12 p-3 rounded-2xl '+ peliculas} onClick={ () => active('peliculas') }>Peliculas</div>
-                    <div className={`cursor-pointer hover:bg-gray-100 h-12 p-3 rounded-2xl ${series}`} onClick={ () => active('series') }>Series</div>
+                    {
+                        opciones.map( (opcion) => (
+                            <div 
+                                className={
+                                    `cursor-pointer hover:bg-gray-100 h-12 p-3 rounded-2xl 
+                                    ${opcion === categoriaActiva ? 'font-bold': ''}`
+                                    }
+                                key={opcion}
+                                onClick={ () => setCategoriaActiva(opcion) }
+                            >
+                                { opcion }
+                            </div>
+                        ) )
+                    }
                 </>
             }
             <Image
@@ -37,13 +48,17 @@ const Busqueda = () => {
                 className='cursor-pointer'
                 onClick={ () => toggleInput() }
             />
-
             {
                 visible &&
-                    <input 
-                        type="text" 
-                        placeholder="Introduce la pelicula a buscar" 
-                        className='w-full ml-2 h-10 p-3 text-[#2F3139] focus:outline-none focus:ring-0' />
+                    <form onSubmit={handleSubmit} className='w-full'>
+                        <input 
+                            type="text" 
+                            placeholder="Introduce la pelicula a buscar"
+                            name="buscar"
+                            // defaultValue={buscar}
+                            onChange={(e) => setBuscar( e.target.value )} 
+                            className='w-full ml-2 h-10 p-3 text-[#2F3139] focus:outline-none focus:ring-0' />
+                    </form>
                 
             }
         </div>
